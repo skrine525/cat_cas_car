8f567a9103d11603ae59c1c56b0a329b3ebee22c
Skrine <tsaregorodtsev03@mail.ru>
2023-08-11 01:12:36 +1000

    Services example

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -3,11 +3,15 @@
 namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Twig\Environment;
 
 class ArticleController extends AbstractController
 {
-    public function homepage()
+    public function homepage(Environment $twig)
     {
+        // $html = $twig->render('articles/homepage.html.twig');
+        // return new Response($html);
+
         return $this->render('articles/homepage.html.twig');
     }
 
--- src/Controller/ArticleLikeController.php
+++ src/Controller/ArticleLikeController.php
@@ -1,21 +1,24 @@
 <?php
 
 namespace App\Controller;
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 
 class ArticleLikeController extends AbstractController
 {
-    public function like($id, $type)
+    public function like($id, $type, LoggerInterface $logger)
     {
         if($type == "like")
         {
             $likes = rand(121, 200);
+            $logger->info('Кто-то лайкнул статью');
         }
         else
         {
             $likes = rand(0, 119);
+            $logger->info('Какая досада, дизлайк');
         }
 
         return $this->json(['likes' => $likes]);
